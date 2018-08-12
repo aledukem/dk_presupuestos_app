3fa71d27f581f1727c5d9fdc58ebe0f82d4d248b
Alejandro.D <aleduque@gmail.com>
2018-08-12 15:38:45 +1000

    spring boot project running

---
+++ src/main/java/dkpc/projects/presupuestos/app/indexController.java
@@ -0,0 +1,13 @@
+package dkpc.projects.presupuestos.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+}
